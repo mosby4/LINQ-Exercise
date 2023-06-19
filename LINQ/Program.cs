43d415d8972512a926f25130b9b18a7557935619
Mosby4 <rufus.hamby@gmial.com>
2023-06-19 11:42:55 -0500

    Done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            { "Super Mario", "Halo", "F1 23", "FortNite", "Dungens and Dragons", };
+
+            IEnumerable<string> nameLength = videoGames.OrderBy(str => str.Length);
+
+            foreach (var name in nameLength)
+            {
+                Console.WriteLine(name);
+            }
+
         }
     }
 }
+/*(Using Method Syntax)
+Create a list of video game names..
+Order the list of games by length of the game name.
+Use the lambda expression in this exercise as well.
+Where() - Filters a sequence of values based on a predicate.
+Select()
+OrderBy() - Sorts the elements of a sequence in ascending order.
+Sum()
+Average()
+Count() - Returns the number of elements in a sequence.
+Min()
+Max()
+Take() - Returns a specified number of contiguous elements from the start of a sequence.
+Append() - Appends a value to the end of the sequence.
+ThenBy() - Performs a subsequent ordering of the elements in a sequence in ascending order.
+*/
